d941671fe74fc74fac5eb251861cf8dc66a3d1bc
james huang <jhuang20@stuy.edu>
2018-01-06 14:16:18 -0500

    g

---
+++ Casino.java
@@ -0,0 +1,22 @@
+import cs1.Keyboard
+public class Casino {
+    //instance var
+    //constructor
+    public Casino() {
+	//BankAccount default values
+
+	
+    }
+    public Casino(int game, int difficulty, int balance, int numOfOpponents) {
+	//modify BankAccount
+	//make var for selected game
+	
+    }
+    //methods
+    public int viewBal() {
+    }
+    public static void main(String[] args) {
+	System.out.println("Welcome to the casino!");
+	
+    }
+}
