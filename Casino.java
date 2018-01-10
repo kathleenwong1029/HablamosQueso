68394931ee337fea560cf85192e0603724b14dfd
kathleenwong1029 <kwong17@stuy.edu>
2018-01-09 20:25:05 -0500

    crazyeight I in casino

--- Casino.java
+++ Casino.java
@@ -6,12 +6,12 @@ public class Casino {
     public Casino() {
 	//BankAccount default values
 
-	
+
     }
     public Casino(int game, int difficulty, int balance, int numOfOpponents) {
 	//modify BankAccount
 	//make var for selected game
-	
+
     }
     //methods
     public int viewBal() {
@@ -30,16 +30,16 @@ public class Casino {
 		System.out.println("The sum of your cards:"+rob.sum());
 	    System.out.println("Ask dealer for other card?");
 	    System.out.println("1 for yes, 2 for no");
-	    
+
 	    u=Keyboard.readInt();
 	    if(u==1) {
 		rob.addtohand();
 		 System.out.println("These are your cards"+rob.printArray(rob.hand));
-		 
+
 	    }
 	    System.out.println("The sum of your cards:"+rob.sum());
 	    }
-	    
+
 	    System.out.println("These are your cards"+rob.printArray(rob.hand));
 	    System.out.println("The sum of your cards:"+rob.sum());
 	    if(rob.sum()>21) {
@@ -61,6 +61,12 @@ public class Casino {
     public void playTexasHoldEm() {
     }
     public void playCrazyEights() {
+      Card placeholder = new Card(3,3);
+      CrazyEight pjelly= new CrazyEight ();
+      pjelly.deal();
+      System.out.println(pjelly.getInstructions());
+      System.out.println(placeholder);
+      pjelly.win(placeholder);
     }
     public static void main(String[] args) {
 	Casino jane=new Casino();
@@ -76,6 +82,9 @@ public class Casino {
 	if (user==1) {
 	    jane.playBlackjack();
 	}
+  if( user==2){
+    jane.playCrazyEights();
+  }
 	System.out.println("1. continue playing \n 2. Exit casino");
 	int cas;
 	cas=Keyboard.readInt();
--- CrazyEight.java
+++ CrazyEight.java
@@ -2,8 +2,9 @@ import cs1.Keyboard;
 public class CrazyEight extends CardGame{
   private int deckLength;
   private boolean isValidMove = false;
-  private boolean winner = false;
-
+  private boolean winner =false;
+  private boolean drawAgain;
+  private int cardNum=5;
   //constructor
   public CrazyEight(){
     super();
@@ -23,10 +24,13 @@ public class CrazyEight extends CardGame{
 
   //draw
   public void drawCard(){
-    Card[] newHand=new Card[deckLength] ;
-    newHand[deckLength-1]=deck[deckLength-1];
+    Card[] newHand=new Card[deckLength+1] ;
+    for(int i=0;i<deckLength;i++){
+      newHand[i]=hand[i];
+    }
+    newHand[deckLength]=deck[cardNum];
     hand=newHand;
-    deckLength +=1;
+    deckLength++;
   }
 
   public boolean win(){
@@ -34,38 +38,56 @@ public class CrazyEight extends CardGame{
   }
 
   public String win(Card other){
-    int a = deckLength;
+    int a = deckLength-1;
     //if player has no usable cards, they are forced to draw
-    while(a>-1){
-      if(hand[a].value==other.value || hand[a].symbol==other.symbol){
+    while(isValidMove==false){
+    while(a>0){
+      if(hand[a].value==8||hand[a].value==other.value||hand[a].symbol==other.symbol){
+        isValidMove=true;
         break;
       }
       if (a==0){
         drawCard();
+        cardNum+=1;
         System.out.println("Here's your new deck:");
       }
       a--;
-    }
+    }}
 
     while(winner == false){
-      System.out.println(hand);
-      System.out.println("Which card would you like to use?");
-
+      System.out.println(printArray(hand));
+      while(!(drawAgain==true)) {
+      System.out.println("Would you like to draw?");
+      String draw =Keyboard.readString();
+      if(draw.equals("yes")){
+        drawCard();
+        cardNum+=1;
+      }
+      System.out.println(printArray(hand));
+      if(draw.equals("no")){
+        drawAgain=true;
+      }
+    }
+    drawAgain=false;
       //allows player to choose which card to play
+      System.out.println("Which card would you like to discard?");
       int user;
       user=Keyboard.readInt();
       if(hand[user].value==other.value || hand[user].symbol==other.symbol){
         Card[] nHand=new Card[deckLength-1];
-
-        for(int i =0;i<user;i--){
+        for(int i =0;i<user;i++){
           nHand[i]=hand[i];
         }
-        for(int i =user+1;i<hand.length;i--){
-          nHand[i]=hand[i];
+        for(int i =hand.length-1;i>user;i--){
+          nHand[i-1]=hand[i];
         }
         hand = nHand;
         deckLength-=1;
-        System.out.println(hand);
+        if(win()==true){
+          winner=true;
+          break;
+        }
+        System.out.println(printArray(hand));
       }
       else{
         System.out.println("Sorry, not a valid move. Try again!");
