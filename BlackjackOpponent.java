bb0b8089ca0f365632b5ed6bcb484ce179b9b776
james huang <jhuang20@stuy.edu>
2018-01-15 22:16:57 -0500

    added comments to BlackjackOpponent.java

--- BlackjackOpponent.java
+++ BlackjackOpponent.java
@@ -1,11 +1,15 @@
 import java.util.ArrayList;
 import java.lang.Math;
 public class BlackjackOpponent extends Blackjack {
-    //first working edition
+    //second working edition
+    /*remaining bugs:
+splitting a hand doesn't quite work. The hand is split, but no comparison is made.
+     */
     public boolean win=true;
     public String name;
     public Card headCard;
-    public ArrayList<Card> deck2=new ArrayList<Card>();//if there is a split
+    public ArrayList<Card> deck2=new ArrayList<Card>();//if there is a split, use this deck
+    //default constructor
     public BlackjackOpponent(String n) {
 	//deal();
 	bet+=(int) (Math.random()*500);
@@ -14,6 +18,7 @@ public class BlackjackOpponent extends Blackjack {
 	
 	
     }
+    //constructor for dealer
     public BlackjackOpponent(int i) {
 	//deal();
         //System.out.println("dealer card:"+hand.get(0));
@@ -21,6 +26,7 @@ public class BlackjackOpponent extends Blackjack {
 	name="dealer";
 	
     }
+    //sums up cards in deck
     public int sum(ArrayList<Card> deck) {
 	int sum=0;//return value
 	for(int i=0; i<deckLength; i++) {//goes thru each card!
@@ -28,11 +34,11 @@ public class BlackjackOpponent extends Blackjack {
 		sum+=10;
 	    }
 	    
-	    else if(hand.get(i).getValue()==1) {
-		if (sum>10) {
+	    else if(hand.get(i).getValue()==1) {//if its an Ace, it can either be 1 or 11
+		if (sum>10) {//if sum is more than 10 already, A must be 1
 		    sum+=1;
 		}
-		else {
+		else {//otherwise, it can be 11
 		    sum+=11;
 		}
 	    }
@@ -45,42 +51,49 @@ public class BlackjackOpponent extends Blackjack {
 	return sum;
 	
     }
+    /***AI that determines what the opponent will do
+	AI(deck, hand, hcard)
+	- deck is the deck of cards that will be used to draw(this allows us to standardize the deck across all instances)
+-hand is the hand that we currently have
+-hcard is the card the dealer is displaying
+postcond: outputs the sum of the hand, adds cards as needed to hand
+    ***/
     public int AI(ArrayList<Card> deck,ArrayList<Card> hand,Card hCard) {
 	headCard=hCard;
 	int sum2=0;
         
 	System.out.println(hand);
-        if (deckLength>1 && hand.get(0).value==hand.get(1).value && sum(deck)<17 && deckLength<3) {
+        if (deckLength>1 && hand.get(0).value==hand.get(1).value && sum(deck)<17 && deckLength<3) {//if there is a pair, and splitting the deck up will lead to more benefit than harm, then it can be split up
 	    System.out.println(hand);
 	    deckLength-=1;
 	    deck2.add(hand.get(0));
 	    hand.remove(0);
 	    sum2=AI(deck, deck2, headCard );
 	}
-	while (sum(hand)<16 || ((headCard.value==10 || headCard.value==1) && sum(hand)<17)) {
+	while (sum(hand)<16 || ((headCard.value==10 || headCard.value==1) && sum(hand)<17)) {//if the dealer's card is a 10 or ace, we are more willing to take a risk with respect to adding cards to our deck
 	    if (deckLength>4) {
-		break;
+		break;//if there are too many cards, end the while loop
 	    }
 	    
-	    addtohand(deck);
-	    System.out.println(name+" added card");
+	    addtohand(deck);//otherwise, add another card
+	    System.out.println(name+" added card");//tells the user that opponent added card
 	    }
-	if((sum(hand)>sum2 && sum(hand)<22)|| sum2>21 ) {
+	if((sum(hand)>sum2 && sum(hand)<22)|| sum2>21 ) {//in a situation where there are two decks(when there is a pair), this compares the two and outputs the higher one
 	    sum2=sum(hand);
 	}
-	if (sum2>21)
-	    win=false;
+	if (sum2>21)//if the final sum is greater than 21
+	    win=false;// we cannot win
 	
-    return sum2;
+	return sum2;//returns sum so we can compare the running of  the AI with 2 deck
     }
-    public Card AIDealer(ArrayList<Card> deck) {
+    public Card AIDealer(ArrayList<Card> deck) {//dealer has simpler logic
 	if (name.equals("dealer")) {
-	    System.out.println("dealer card:"+hand.get(0));
+	    System.out.println("dealer card:"+hand.get(0));//must display first card
 	    headCard=hand.get(0);
 	}
 	
         
-	while (sum(deck)<17) {
+	while (sum(deck)<17) {//must add cards if below 17
 	    if (deckLength>4) {
 		break;
 	    }
@@ -93,6 +106,6 @@ public class BlackjackOpponent extends Blackjack {
     }
     
     public String toString() {
-	return name+"'s  hand:"+hand+"\n sum of cards:"+sum(hand);
+	return name+"'s  hand:"+hand+"\n sum of cards:"+sum(hand);//shows us the other player's cards
     }
 }
